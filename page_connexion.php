47e43f2fa441adcc276110d9e56ed9231b7d066f
el706 <79935941+el706@users.noreply.github.com>
2023-05-25 11:47:49 +0200

    Add files via upload

--- page_connexion.php
+++ page_connexion.php
@@ -1,43 +1,31 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 <head>
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-	<style>
-		header{
-			width: 100%;
-			height: 100px;
-			left: 0px;
-			top: 0px;
-			background: #D9D9D9;
-		}
-		a{
-			 all: unset;
-		}
-	</style>
+	<link rel="stylesheet" href="./style/page_connexion.css">
+	
 </head>
 <body style="margin: 0;">
-	<header>
-		<div align=left style="vertical-align: middle;">
+<header>
+		<div id="logo">
 		    <a href=page_accueil2.html><img style="max-height: 100px;" src="logo.png" alt="Logo site"></a>
+			
 		</div>
-		<div align=right style="vertical-align: middle;position:absolute;right:40px;top:25px;height:50px;line-height: 50px;">
-			<a style="vertical-align: middle;font-size: 30px;">Jeune</a>
+		<div id="texte">Je donne de la valeur à mon engagement</div>
+		
+		<div id="bouton">
+			<a href="voir_profil.php">Jeune</a>
 		</div>
 	</header>
-
-	<?php
-session_start();
-echo $_SESSION['error'];
-$_SESSION['message'] = '';
-?>
-
 	<main>
+	<div class="gauche  "> <fieldset>
 		<form action="verification_connexion.php" method="post">
-			<p>Email <input name="email" type="email" id="email"></p>
-			<p>Mot de passe <input name="password" type="password" id="password"></p>
-			<input type="submit" value="connexion">
+		<p>  <span class="roseG">Connectez vous </span></p>
+			<p>Email <br><input  size="50" placeholder="arthur.martin@gmail.com"name="email" type="email"></p>
+			<p>Mot de passe  <br><input size="50"  placeholder="****" name="mdp" type="password"></p>
+			<div class="centre"><input  class="submit "type="submit" value="Se connecter"> 
 		</form>
-		<p>Vous avez pas de compte ? <a style="color:blue" href="page_creation_compte.php">Inscrivez-vous</a></p>
+		<p>Vous avez pas de compte ? <a style="color:blue" href="page_creation_compte.php"> <span class="rose">Inscrivez-vous</a></p> </div> </div>
 	</main>
 </body>
 
