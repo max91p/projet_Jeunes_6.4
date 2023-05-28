adffcf1b50d2904ce42800c10ddc1b447526e071
maxime <64915894+max91p@users.noreply.github.com>
2023-05-28 18:42:14 +0200

    css voir profil + modif accueil compte

--- voir_profil.php
+++ voir_profil.php
@@ -2,39 +2,54 @@
 <html>
 <head>
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-	<style>
-		header{
-			width: 100%;
-			height: 100px;
-			left: 0px;
-			top: 0px;
-			background: #D9D9D9;
-		}
-		a{
-			 all: unset;
-		}
-	</style>
+	<link rel="stylesheet" href="./style/voir_profil.css">
+	<title>Profil</title>
 </head>
-<body style="margin: 0;">
+<body>
 	<header>
-		<div align=left style="vertical-align: middle;">
-			<a href=page_accueil2.html><img style="max-height: 100px;" src="logo.png" alt="Logo site"></a>
+		<div id="logo">
+			<a href=page_accueil2.html><img src="./media/logo.png" alt="Logo site"></a>
 		</div>
-		<div align=right style="vertical-align: middle;position:absolute;right:40px;top:25px;height:50px;line-height: 50px;">
-			<a style="vertical-align: middle;font-size: 30px;">Jeune</a>
+		<div id="texte"><p>Je donne de la valeur à mon engagement</p></div>
+		<div id="bouton">
+			Jeune
 		</div>
 	</header>
-	<br>
-	<br>
 	<main>
-		<a href="accueil_compte.html"><--</a>
-		<p>Votre compte</p>
-		<p>Nom<br>Dupont</p>
-		<p>Prénom<br>Martin</p>
-		<p>Date de naissance<br>01/01/1970</p>
-		<p>Email<br>martin.dupont@gmail.com</p>
-		<a href="page_modifier_compte.php">Modifier</a>
+		<a href="accueil_compte.html"><img id="home" src="./media/home.png" alt="home"></a>
+		<div id="content">
+			<div id="up">
+				<div id="up-left">Votre compte</div>
+				<div id="up-right">
+					<a href="page_modifier_compte.php"><img id="edit" src="./media/edit.png" alt="edit"></a>
+				</div>
+			</div>
+			
+			<p class="title">Nom</p>
+			<p>Dupont</p>
+			<p class="title">Prénom</p>
+			<p>Martin</p>
+			<p class="title">Date de naissance</p>
+			<p>01/01/1970</p>
+			<p class="title">Email</p>
+			<p>martin.dupont@gmail.com</p>
+			
+		</div>
+		
 	</main>
 </body>
+
+<script>
+	// recupère la largeur du navigateur et cache le texte si la largeur est inférieure à 865px
+	window.addEventListener('resize', function() {
+		var browserWidth = window.innerWidth;
+
+		if (browserWidth < 865) { 
+			document.getElementById('texte').style.display = 'none';
+		} else {
+			document.getElementById('texte').style.display = 'inline-block';
+		}
+	});
+</script>
 
 </html>
