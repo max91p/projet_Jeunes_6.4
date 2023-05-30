aa555402065c4d8095435dd677d3a3487636fa14
emmafeneau <120452385+emmafeneau@users.noreply.github.com>
2023-05-30 11:43:34 +0200

    Update voir_profil.php
    ajout php

--- voir_profil.php
+++ voir_profil.php
@@ -15,6 +15,36 @@
 			Jeune
 		</div>
 	</header>
+			<?php
+
+		session_start();
+		$username = $_SESSION['email'];
+		//session_unset();
+		//session_destroy();
+
+// récupération de toutes les lignes du fchier dans un tableau
+$data = file('people.csv');
+
+//echo $username;
+//débug
+//var_dump($_POST);
+
+foreach ($data as $item) {
+    //on décide chaque ligne comme étant du csv
+    $csv = str_getcsv($item, ';');
+    //var_dump($item);
+    if ($username == $csv[3]) {
+
+		$firstname = $csv[1];
+		$lastname = $csv[0];
+		$birth = $csv[2];
+		$username = $csv[3];
+
+		//echo "utilisateur trouvé";
+		break;
+    }
+}
+?>
 	<main>
 		<a href="accueil_compte.html"><img id="home" src="./media/home.png" alt="home"></a>
 		<div id="content">
@@ -25,14 +55,13 @@
 				</div>
 			</div>
 			
-			<p class="title">Nom</p>
-			<p>Dupont</p>
-			<p class="title">Prénom</p>
-			<p>Martin</p>
-			<p class="title">Date de naissance</p>
-			<p>01/01/1970</p>
-			<p class="title">Email</p>
-			<p>martin.dupont@gmail.com</p>
+			<a href="accueil_compte.html"></a>
+		<p>Votre compte</p>
+		<p class='title'>Nom<br><?php echo $lastname?></p>
+		<p class='title'>Prénom<br><?php echo $firstname?></p>
+		<p class='title'>Date de naissance<br><?php echo $birth?></p>
+		<p class='title'>Email<br><?php echo $username?></p>
+		<a href="page_modifier_compte.php">Modifier</a>
 			
 		</div>
 		
