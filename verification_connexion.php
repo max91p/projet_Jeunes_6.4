e2c8a5f112c4ff0de2237e94115a1fda263ef311
emmafeneau <120452385+emmafeneau@users.noreply.github.com>
2023-05-30 11:23:22 +0200

    Add files via upload

--- verification_connexion.php
+++ verification_connexion.php
@@ -1,27 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 session_start();
 
 $username = $_POST['email'];
 $password = $_POST['password'];
 
+// récupération de toutes les lignes du fchier dans un tableau
 $data = file('people.csv');
 
+//débug
 var_dump($_POST);
 
 foreach ($data as $item) {
+    //on décide chaque ligne comme étant du csv
     $csv = str_getcsv($item, ';');
     var_dump($item);
-    if ($username == $csv[3] && $password == $csv[4]) {
+    //if ($username == $csv[3] && $password == $csv[4]) {
+    if ($username == $csv[3] && password_verify($password, $csv[4]) == true) {
+        $_SESSION['email'] = $username;
         $_SESSION["prenom_jeune"] = $csv[0];
         $_SESSION["nom_jeune"] = $csv[1];
-        $_SESSION["email_jeune"] = $username;
+        //$_SESSION["email_jeune"] = $username;
         $_SESSION["naissance"] = $csv[2];
         $_SESSION["mdp"] = $password;
-        header('Location: accueil_compte.html');
+        ?>
+            <p>
+                 compte valide
+            </p>
+
+<meta http-equiv="refresh" content="3; URL=accueil_compte.html">
+        <?php 
+        //header('Location: accueil_compte.html');
         exit();
     }
 }
@@ -29,5 +48,9 @@ header('Location: page_connexion.php');
 $_SESSION['error'] = 'Login ou mot de passe incorrect';
 
 ?>
+</body>
+
+</html>
+
 
 
