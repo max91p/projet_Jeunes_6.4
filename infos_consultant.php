d0a05ec5fb8ec45d9935bdd7600f2cd31aabd293
el706 <79935941+el706@users.noreply.github.com>
2023-06-01 13:20:25 +0200

    Add files via upload
    ajout envoie mail consultant + css

--- infos_consultant.php
+++ infos_consultant.php
@@ -4,19 +4,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-	<style>
-		header{
-			width: 100%;
-			height: 100px;
-			left: 0px;
-			top: 0px;
-			background: #D9D9D9;
-		}
-		a{
-			 all: unset;
-		}
-	</style>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+	<link rel="stylesheet" href="./style/infos_consultant.css">
+
 </head>
 <body style="margin: 0;">
     <?php
@@ -45,23 +35,43 @@
         
     ?>
 	<header>
-        <div align=left style="vertical-align: middle;">
-            <a href=page_accueil2.html><img style="max-height: 100px;" src="logo.png" alt="Logo site"></a>
-        </div>
-        <div align=right style="vertical-align: middle;position:absolute;right:40px;top:25px;height:50px;line-height: 50px;">
-            <a href="voir_profil.php" style="vertical-align: middle;font-size: 30px;">Jeune</a>
-        </div>
+		<div id="logo">
+		    <a href=page_accueil2.html><img src="./media/logo.png" alt="Logo site"></a>
+			
+		</div>
+		<div id="texte">Je donne de la valeur à mon engagement</div>
+		
+		<div id="bouton">
+			<a href="voir_profil.php">Jeune</a>
+		</div>
 	</header>
 	<main>
-        <a href="selection_envoi_ref.php"><--</a>
-        <h2 style="text-align:center;">Renseigner les informations du consultant</h2>
-        <br>
-        <div style="border:2px solid black;margin-right:35%;margin-left:35%;padding :5px;">
+        <a href="selection_envoi_ref.php"></a>
+        <div id="content">
+        <fieldset>
+			<legend>Renseigner les informations du consultant </legend>
+       
+  
             <form action="envoi_ref_consultant.php" method="post">
-                <p>Nom <br><input name="nom_consultant" type="text"></p>
-                <p>Prénom <br><input name="prenom_consultant" type="text"></p>
-                <p>Email <br><input name="email_consultant" type="email"></p>
-                <input type="submit" name= 'envoyer_mail_consultant' value="Envoyer">
+                <div class="form">
+                    <label for="nom_consultant">Nom</label>
+                    <input id="nom_consultant" name="nom_consultant" type="text"  required>
+                </div>
+
+                <div class="form">
+                    <label for="prenom_consultant">Prénom</label>
+                    <input id="prenom_consultant" name="prenom_consultant" type="text" required>
+                </div>
+               
+                <div class="form">
+                    <label for="email_consultant">Email</label>
+                    <input id="email_consultant" name="email_consultant" type="email" required>
+                </div>
+
+                <div id="submit" class="form"> 
+                    <input type="submit"  name= 'envoyer_mail_consultant'value="Envoyer">
+                </div>
+            
             </form>
         </div>
 	</main>
