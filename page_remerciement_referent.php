4327f19a9e73f2c3a6e3de8111d0c72b1ed21543
el706 <79935941+el706@users.noreply.github.com>
2023-06-01 14:56:09 +0200

    Ajout liste demande + consult demande + css

--- page_remerciement_referent.php
+++ page_remerciement_referent.php
@@ -5,41 +5,39 @@
 <html>
 <head>
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-	<style>
-		header{
-			width: 100%;
-			height: 100px;
-			left: 0px;
-			top: 0px;
-			background: #D9D9D9;
-		}
-		a{
-			 all: unset;
-		}
-	</style>
+	<link rel="stylesheet" href="./style/page_remerciment_referent.css">
+
+
 </head>
 <body style="margin: 0;">
     <?php
         $nom_jeune=$_SESSION['nom_jeune'];
         $prenom_jeune=$_SESSION['prenom_jeune'];
         $milieu=$_SESSION['milieu'];
-        echo 
+        echo 	
 				"<header>
-					<div align=left style='vertical-align: middle;'>
-						<a href=page_accueil2.html><img style='max-height: 100px;' src='media/logo.png' alt='Logo site'></a>
+					<div id="logo">
+						<a href=page_accueil2.html><img src="./media/logo.png" alt="Logo site"></a>
+						
 					</div>
-					<div align=right style='vertical-align: middle;position:absolute;right:40px;top:25px;height:50px;line-height: 50px;'>
-						<a style='vertical-align: middle;font-size: 30px;'>Référent</a>
+					<div id="texte">Je confirme la valeur de ton engagement</div>
+					
+					<div id="bouton">
+						<a href="voir_profil.php">Referent </a>
 					</div>
 				</header>
 				<main>
-                    <div style='border:2px solid #ABDA4E;margin:50px;padding-right:20px;padding-left:20px'>
+					<div id="content">
+					<fieldset>
 					    <h2>Merci !</h2>
                         <p>Vos ajouts sur cette référence dans le milieu \"$milieu\" ont été envoyés à $prenom_jeune $nom_jeune par mail.<br><br>
                         $prenom_jeune $nom_jeune pourra envoyer ses références à des recruteurs et ils auront accès à votre identité et adresse mail.</p>
                     </div>
-				</main>";
-	?>
+
+					</fieldset>
+				</div>
+				</main> ";
+				?>
 	
 </body>
 
