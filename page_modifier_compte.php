8aae22e9940d3cbe130b058a3dd0f1af66254f8c
Alexandra <103836628+abaivel@users.noreply.github.com>
2023-06-11 12:34:21 +0200

    Update page_modifier_compte.php
    Le champ pour le mot de passe doit être vide (sinon le mot de passe enregistré est celui crypté, qui sera recrypté)

--- page_modifier_compte.php
+++ page_modifier_compte.php
@@ -58,7 +58,7 @@
 			</div>
 			<div class="form">
 				<label for="password">Mot de passe</label>
-				<input id="password" value="<?php echo $password; ?>" name="password" type="password">
+				<input id="password" name="password" type="password">
 			</div>
 			<div id="submit" class="form"> 
 				<input type="submit" value="Enregistrer">
